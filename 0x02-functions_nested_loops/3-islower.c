24c8c035ecf3bf611dc110cf12247f8475a25672
jeffery mutuku <jefferymutuku283@gmail.com>
2023-07-18 13:25:21 -0700

    wow

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * Checks for lowercase letters
+ * c is the one to be cheched
+ */
+int _islower(int c);
+{
+	if(c>='a'&&c<='Z')
+		return(1);
+	else
+		return(0);
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * checks for alphabet character if lowercase or uppercase
+ * c is the letter
+ */
+int _isalpha(int c);
+{
+	return((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
