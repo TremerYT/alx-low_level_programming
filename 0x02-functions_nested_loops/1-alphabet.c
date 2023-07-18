e3355f94e0ee521955dfdae89c5b5424679e1d2d
jeffery mutuku <jefferymutuku283@gmail.com>
2023-07-18 14:28:59 -0700

    wow

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
+
 /**
- * print_alphabet-prints alphabet in lower case
+ * print_alphabet - print all alphabet in lowercase
  */
+
 void print_alphabet(void)
 {
 	char letter;
-	
-	for (letter = 'a'; letter<='z'; letter++)
+
+	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 
 	_putchar('\n');
-
 }
