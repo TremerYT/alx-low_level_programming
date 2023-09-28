92957673537926937939cceed9eba1494605157f
Jeffery Mutuku <jefferymutuku283@gmail.com>
2023-09-28 04:16:03 -0700

    0x14

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,16 +10,16 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int i, count = 0;
-	unsigned long int current;
-	unsigned long int exclusive = n ^ m;
+	int i, c = 0;
+	unsigned long int a;
+	unsigned long int b = n ^ m;
 
 	for (i = (sizeof(unsigned long int) * 8) - 1; i >= 0; i--)
 	{
-		current = exclusive >> i;
-		if (current & 1)
-			count++;
+		a = b >> i;
+		if (a & 1)
+			c++;
 	}
 
-	return (count);
+	return (c);
 }
